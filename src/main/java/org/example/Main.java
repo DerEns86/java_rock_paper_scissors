4f185b92207f3590b01c3bd7539d5409f720df17
Viktor Ens <v.ens86@yahoo.de>
2024-04-18 16:40:28 +0200

    finish the challenge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(rps("rock", "scissors"));
+    }
+
+    public static String rps(String p1, String p2) {
+        if (p1.equals(p2)) {
+            return "Draw!";
+        }
+        if (p1.equals("rock") && p2.equals("scissors")) {
+            return "Player 1 won!";
+        }
+        if (p1.equals("scissors") && p2.equals("paper")) {
+            return "Player 1 won!";
+        }
+        if (p1.equals("paper") && p2.equals("rock")) {
+            return "Player 1 won!";
+        }
+        return "Player 2 won!";
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests Player 1 won!");
+        assertEquals("Player 1 won!", Main.rps("rock", "scissors"));
+        assertEquals("Player 1 won!", Main.rps("scissors", "paper"));
+        assertEquals("Player 1 won!", Main.rps("paper", "rock"));
+    }
+    @Test
+    public void test2() {
+        System.out.println("Fixed Tests Player 2 won!");
+        assertEquals("Player 2 won!", Main.rps("scissors", "rock"));
+        assertEquals("Player 2 won!", Main.rps("paper", "scissors"));
+        assertEquals("Player 2 won!", Main.rps("rock", "paper"));
+    }
+    @Test
+    public void test3() {
+        System.out.println("Fixed Tests Draw!");
+        assertEquals("Draw!", Main.rps("scissors", "scissors"));
+        assertEquals("Draw!", Main.rps("paper", "paper"));
+        assertEquals("Draw!", Main.rps("rock", "rock"));
+    }
+
 }
